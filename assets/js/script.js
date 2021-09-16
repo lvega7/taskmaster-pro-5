e76fe09ca507e316df5e4deddaecf250e6d66b15
Lissette Vega <lissettevega@Lissettes-MacBook-Pro.local>
2021-09-15 23:54:24 -0400

    add calendar and colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -44,6 +46,27 @@ var loadTasks = function() {
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
+
+var auditTask=function(taskEl){
+ // get date from task element
+ var date = $(taskEl).find("span").text().trim();
+
+ // convert to moment object at 5:00pm
+ var time = moment(date, "L").set("hour", 17);
+
+ // remove any old classes from element
+ $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+ // apply new class if task is near/over due date
+ if (moment().isAfter(time)) {
+   $(taskEl).addClass("list-group-item-danger");
+ }
+ else if (Math.abs(moment().diff(time, "days")) <= 2) {
+  $(taskEl).addClass("list-group-item-warning");
+ }
+
+};
+
 
 $(".card .list-group").sortable({
   connectWith: $(".card .list-group"),
@@ -155,7 +178,7 @@ $(".list-group").on("click", "p", function() {
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type=text]", function(){
+$(".list-group").on("change", "input[type=text]", function(){
   // get current text
   var date = $(this)
     .val()
@@ -183,6 +206,9 @@ $(".list-group").on("blur", "input[type=text]", function(){
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 
@@ -202,12 +228,24 @@ $(".list-group").on("click", "span", function() {
   // swap out elements
   $(this).replaceWith(dateInput);
 
+    // enable jquery ui datepicker
+    dateInput.datepicker({
+      minDate: 1,
+      onClose:function(){
+        // when calendar is closed, force a "change" event on the dateInput
+        $(this).trigger("change");
+      }
+    });
+
   // automatically focus on new element
   dateInput.trigger("focus");
 });
 
 
+$("#modalDueDate").datepicker({
+  minDate:1
 
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
